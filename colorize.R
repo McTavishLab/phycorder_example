2702c8bd2427a0a6bc071eb3caa252c4caf2a009
Emily Jane McTavish <ejmctavish@gmail.com>
2020-08-04 09:15:59 -0700

    wd

--- colorize.R
+++ colorize.R
@@ -1,7 +1,21 @@
 library(phytools)
 
-#treefile = '/home/ejmctavish/projects/otapi/data_physcraper/data/pg_2827_tree6577/run_pg_2827tree6577_run4/RAxML_bestTree.2020-07-31'
-#otu_info_file = '/home/ejmctavish/projects/otapi/data_physcraper/data/pg_2827_tree6577/outputs_pg_2827tree6577/otu_info_pg_2827tree6577.csv'
+## set working directory to path to https://github.com/McTavishLab/physcraper_example
+setwd("~/projects/otapi/data_physcraper")
+
+#treefile = 'data/pg_2827_tree6577/run_pg_2827tree6577_run4/RAxML_bestTree.2020-07-31'
+#otu_info_file = 'data/pg_2827_tree6577/outputs_pg_2827tree6577/otu_info_pg_2827tree6577.csv'
+
+#treefile = 'pg_55_local_new/outputs_pg_55tree5864_ndhf/physcraper_pg_55tree5864_ndhf.tre'
+#otu_info_file = 'pg_55_local_new/outputs_pg_55tree5864_ndhf/otu_info_pg_55tree5864_ndhf.csv'
+
+
+#treefile = 'ot_350/outputs_ot_350Tr53297/physcraper_ot_350Tr53297.tre'
+#otu_info_file = 'ot_350/outputs_ot_350Tr53297/otu_info_ot_350Tr53297.csv'
+
+
+treefile = 'pg127/outputs_pg_127tree804/physcraper_pg_127tree804.tre'
+otu_info_file = 'pg127/outputs_pg_127tree804/otu_info_pg_127tree804.csv'
 
 
 phytree = read.tree(treefile)
@@ -41,11 +55,11 @@ for (lab in pruned[["tip.label"]]) {
 }
 
 
-plotBranchbyTrait(pruned, new, mode=c("tips"), show.tip.label = TRUE, palette=colorRampPalette(c("black", "red")), type='fan')
+plotBranchbyTrait(pruned, new, mode=c("tips"),legend=FALSE, show.tip.label = FALSE, palette=colorRampPalette(c("black", "red")), type='fan')
 
 pruned[["tip.label"]] <- spp_labels
 names(new) <- pruned[["tip.label"]]
-plotBranchbyTrait(pruned, new, mode=c("tips"), legend=FALSE, show.tip.label = TRUE, palette=colorRampPalette(c("black", "red")), cex=0.3, edge.width=0.5)
+plotBranchbyTrait(pruned, new, mode=c("tips"), legend=FALSE, show.tip.label = FALSE, palette=colorRampPalette(c("black", "red")), cex=0.5, edge.width=3, type='unrooted')
 
 
 
